2b121a1dd142e47b5204796b599c434056870672
Ravi Tambade <ravi.tambade@transflower.in>
2025-03-13 12:36:46 +0530

    pointer to structure  stack and heap

--- Day3/main5.c
+++ Day3/main5.c
@@ -1,6 +1,8 @@
 //Console Menu Driven Student Information management system
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define MAX 50
 #define SIZE 3
 
@@ -8,6 +10,9 @@ struct Student{
     char name[50];
     int age;
     float marks;
+    char contactNumber[10];
+    char email[50];
+    char address[200];
 };
  
 struct Student students[SIZE];  //array of 10 students
@@ -83,6 +88,43 @@ int main() {
     ptr=&ch;  //implicit typcasting
     printf("\n Value of *ptr: %c\n", *(char *)ptr);
 
+   
+
     int * ptrNewPointer=(int *)ptr;
     printf("\n Value of *ptrNewPointer: %c\n", *ptrNewPointer);
+
+    //dynamic memory allocation
+    int * ptrStockAvailable=NULL;
+    ptrStockAvailable=(int *)malloc(sizeof(int));
+    *ptrStockAvailable=100;
+  
+    int byteInt=sizeof(int);
+    int byteFloat=sizeof(float);
+    int byteChar=sizeof(char);
+    int bytesStudentStruct=sizeof(std1);
+
+    printf("Size of int: %d\n", byteInt);
+    printf("Size of float: %d\n", byteFloat);
+    printf("Size of char: %d\n", byteChar);
+    printf("Size of Struct Student std1: %d\n", bytesStudentStruct);
+
+    struct Student std1;
+    strcpy(std1.name,"Reena");
+    std1.age=21;
+    std1.marks=89.5;
+    strcpy(std1.contactNumber,"987657675");
+    strcpy(std1.email,"reena.sharma@gmail.com");
+    strcpy(std1.address,"Mumbai, Maharashtra, India");
+
+    struct Student * ptrStudent=NULL;
+    ptrStudent=(struct Student *)malloc(sizeof(struct Student));
+    strcpy(ptrStudent->name,"Ravi");
+    ptrStudent->age=49;
+    ptrStudent->marks=80.5;
+    strcpy(ptrStudent->contactNumber,"9881735801");
+    strcpy(ptrStudent->email,"ravi.tambade@gamil.com");
+    strcpy(ptrStudent->address,"Pune, Maharashtra, India");
+
+
+
 }
